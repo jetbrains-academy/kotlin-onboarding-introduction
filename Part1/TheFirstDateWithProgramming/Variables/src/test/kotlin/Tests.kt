770748692559397bc38cc992a32516d7b1d272b1
Anastasiia.Birillo <nbirillo@mail.ru>
2022-07-06 19:48:17 +0200

    Add tests for checking if a variable was added into the code

--- Part1/TheFirstDateWithProgramming/Variables/src/test/kotlin/Tests.kt
+++ Part1/TheFirstDateWithProgramming/Variables/src/test/kotlin/Tests.kt
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import util.runMainFunction
+import java.io.File
 
 class Test {
     @Test
@@ -11,4 +12,29 @@ class Test {
             runMainFunction(::main)
         )
     }
+
+    @Test
+    fun testVariablesInSolution() {
+        val taskDirectoryPath = System.getProperty("user.dir")
+        val sourceCodeFile = File("$taskDirectoryPath/src/main/kotlin/Main.kt")
+        if (sourceCodeFile.exists()) {
+            val variables = listOf("firstUserAnswer", "secondUserAnswer", "thirdUserAnswer")
+            val content = sourceCodeFile.readText()
+            for (variable in variables) {
+                val baseDef = variableDefTemplateBase(variable)
+                val defWithType = variableDefTemplateWithType(variable)
+                if (!(baseDef in content || defWithType in content)) {
+                    error("The code should contains a definition of the $variable variable! " +
+                            "Please, add <$baseDef> or <$defWithType> code in your solution.")
+                }
+            }
+        } else {
+            // TODO: log some errors?
+            error("Internal course error!")
+        }
+    }
+
+    private fun variableDefTemplateBase(variableName: String) = "val $variableName = \"\""
+
+    private fun variableDefTemplateWithType(variableName: String) = "val $variableName: String = \"\""
 }
