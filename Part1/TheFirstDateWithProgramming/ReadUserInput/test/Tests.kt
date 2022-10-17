f100b41b7c18ef555916301c9ca359481e7b0071
Anastasiia.Birillo <nbirillo@mail.ru>
2022-10-17 11:25:27 +0200

    Fix tests

--- Part1/TheFirstDateWithProgramming/ReadUserInput/test/Tests.kt
+++ Part1/TheFirstDateWithProgramming/ReadUserInput/test/Tests.kt
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import util.Util
 import util.Util.DEFAULT_USER_INPUT
 import util.runMainFunction
 
@@ -7,12 +8,12 @@ class Test {
     @Test
     fun testSolution() {
         Assertions.assertEquals(
-            "Hello! I will ask you several questions.\n" +
-                    "Please answer all of them and be honest with me!\n" +
-                    "What is TROTEN?\n" +
-                    "How did you spend your graduation?\n" +
-                    "Why does a spider need eight legs?\n",
-            runMainFunction(::main, "$DEFAULT_USER_INPUT\n$DEFAULT_USER_INPUT\n$DEFAULT_USER_INPUT\n")
+            "Hello! I will ask you several questions.${Util.newLineSeparator}" +
+                    "Please answer all of them and be honest with me!${Util.newLineSeparator}" +
+                    "What is TROTEN?${Util.newLineSeparator}" +
+                    "How did you spend your graduation?${Util.newLineSeparator}" +
+                    "Why does a spider need eight legs?${Util.newLineSeparator}",
+            runMainFunction(::main, "$DEFAULT_USER_INPUT${Util.newLineSeparator}$DEFAULT_USER_INPUT${Util.newLineSeparator}$DEFAULT_USER_INPUT${Util.newLineSeparator}")
         )
     }
 }
