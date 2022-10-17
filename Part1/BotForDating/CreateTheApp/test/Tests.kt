9653fa1ea1703a42b21591ffb8192ae8e2bf5a84
Anastasiia Birillo <nbirillo@mail.ru>
2022-10-17 13:52:55 +0300

    Fix tests and add gifs (#42)

--- Part1/BotForDating/CreateTheApp/test/Tests.kt
+++ Part1/BotForDating/CreateTheApp/test/Tests.kt
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import util.Util
 import util.Util.DEFAULT_USER_INPUT
 import util.runMainFunction
 
@@ -7,12 +8,12 @@ class Test {
     @Test
     fun testSolution() {
         Assertions.assertEquals(
-            "Hello! I'm glad to meet you, let me get to know you better! What is your name?\n" +
+            "Hello! I'm glad to meet you, let me get to know you better! What is your name?${Util.newLineSeparator}" +
                     "Nice to meet you, $DEFAULT_USER_INPUT! My name is Kotlin Bot! I am a young programming " +
-                    "language and was created in 2010. How old are you?\n" +
+                    "language and was created in 2010. How old are you?${Util.newLineSeparator}" +
                     "$DEFAULT_USER_INPUT is great! I hope you successfully complete this course! " +
-                    "Anyone can learn programming at any age!\n",
-            runMainFunction(::main, "$DEFAULT_USER_INPUT\n$DEFAULT_USER_INPUT\n")
+                    "Anyone can learn programming at any age!${Util.newLineSeparator}",
+            runMainFunction(::main, "$DEFAULT_USER_INPUT${Util.newLineSeparator}$DEFAULT_USER_INPUT${Util.newLineSeparator}")
         )
     }
 }
