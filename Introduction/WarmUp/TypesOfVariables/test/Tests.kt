1fdd545836678045108c5a6ff79f5fbca0a12a4c
anastasia.birillo <anastasia.i.birillo@gmail.com>
2022-12-20 17:42:29 +0100

    Fix spaces

--- Introduction/WarmUp/TypesOfVariables/test/Tests.kt
+++ Introduction/WarmUp/TypesOfVariables/test/Tests.kt
@@ -26,17 +26,19 @@ class Test {
         val wordLength = 4
         val secretExample = "ACEB"
         val maxAttemptsCount = 3
-        val expectedOutput = "Welcome to the game! $newLineSymbol" +
+        val expectedOutput = "Welcome to the game!$newLineSymbol" +
                 newLineSymbol +
                 "Two people play this game, one chooses a word (a sequence of letters), " +
                 "the other guesses it. In this case, the computer chooses the word: " +
                 "a sequence of $wordLength letters (for example, $secretExample). " +
                 "Several attempts are given to guess it (the max number is $maxAttemptsCount). " +
                 "For each attempt, the number of complete matches (letter and position) " +
-                "and partial matches (letter only) is reported. $newLineSymbol" +
+                "and partial matches (letter only) is reported.$newLineSymbol" +
                 newLineSymbol +
                 "For example, for the BCDF guess (with $secretExample as the hidden word), " +
                 "there will be 1 full match (C), 1 partial match (B).$newLineSymbol"
-        Assertions.assertEquals(expectedOutput, runMainFunction(::main))
+        Assertions.assertEquals(expectedOutput, trimOutput(runMainFunction(::main)))
     }
+
+    private fun trimOutput(output: String) = output.lines().joinToString(newLineSymbol) { it.trim() }
 }
