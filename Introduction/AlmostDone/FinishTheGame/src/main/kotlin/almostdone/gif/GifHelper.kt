12b5be9d3863005a4fec36aff57c8c37fd66c06d
Ilya Vlasov <ilya.vlasov@jetbrains.com>
2023-07-03 19:00:15 +0400

    feat: add AlmostDone runners

---
+++ Introduction/AlmostDone/FinishTheGame/src/main/kotlin/almostdone/gif/GifHelper.kt
@@ -0,0 +1,24 @@
+package almostdone.gif
+
+import chooseFilter
+import choosePicture
+import getPicture
+import photoshop
+
+fun runChooseFilter() {
+    chooseFilter()
+}
+
+fun runChoosePicture() {
+    choosePicture()
+}
+
+fun runGetPicture() {
+    getPicture()
+}
+
+fun runTrimmedPicture(): Nothing = TODO()
+
+fun runPhotoshop() {
+    photoshop()
+}
