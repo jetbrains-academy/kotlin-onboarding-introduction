2c485278de0ac3f353153e2d23da7e103414a938
anastasia.birillo <anastasia.i.birillo@gmail.com>
2023-12-13 20:41:47 +0100

    Fix tests

--- MastermindAdvanced/generateSecretFunction/test/MainClass.kt
+++ MastermindAdvanced/generateSecretFunction/test/MainClass.kt
@@ -1,7 +1,7 @@
 import org.jetbrains.academy.test.system.core.models.classes.TestClass
 
 internal val mainClass = TestClass(
-    classPackage = "jetbrains.kotlin.course.warmup",
+    classPackage = "jetbrains.kotlin.course.mastermind.advanced",
     customMethods = listOf(
         isCompleteMethod,
         countExactMatchesMethod,
