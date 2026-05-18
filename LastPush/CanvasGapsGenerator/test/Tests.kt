551d7e72ff9b67badd600e5530fd3046797141e7
aleksandra.valerianova <sasha.val2006@gmail.com>
2026-05-18 23:27:55 +0300

    Resolved problem with test on Windows

--- LastPush/CanvasGapsGenerator/test/Tests.kt
+++ LastPush/CanvasGapsGenerator/test/Tests.kt
@@ -77,7 +77,7 @@ class Test {
         val userMethod = mainClass.findMethod(mainClazz, dropTopLineMethod)
         val patternWidth = getPatternWidth(image)
         val patternHeight = getPatternHeight(image)
-        val actualResult = userMethod.invokeWithArgs(image, 1, patternHeight, patternWidth, clazz = mainClazz).toString()
+        val actualResult = userMethod.invokeWithArgs(image.replaceLineSeparator(), 1, patternHeight, patternWidth, clazz = mainClazz).toString()
         val error = "The method ${dropTopLineMethod.name} with arguments image=${newLineSymbol}$image${newLineSymbol}, width=1, patternHeight=$patternHeight, patternWidth=$patternWidth should return$newLineSymbol$expected${newLineSymbol}But it returns$newLineSymbol$actualResult"
         Assertions.assertEquals(
             expected.replaceLineSeparator(),
